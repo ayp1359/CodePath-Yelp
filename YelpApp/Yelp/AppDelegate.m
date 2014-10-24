54680338f8237f5efc0ede4caa2123dd24f9cc80
ayp1359 <ayp1359@yahoo.com>
2014-10-24 13:27:23 -0700

    autolayout and AFNetworking stuff

--- YelpApp/Yelp/AppDelegate.m
+++ YelpApp/Yelp/AppDelegate.m
@@ -14,8 +14,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
-    self.window.rootViewController = [[MainViewController alloc] init];
+  
+  self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[MainViewController alloc]init]];
+  
+   // self.window.rootViewController = [[MainViewController alloc] init];
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
